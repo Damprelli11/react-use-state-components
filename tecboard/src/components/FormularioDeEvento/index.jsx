c74849f481472c79b5e192ba4b2990908080ae7c
Damprelli11 <renandamprelli@gmail.com>
2025-11-02 21:21:30 -0300

    feat: compondo componentes

--- tecboard/src/components/FormularioDeEvento/index.jsx
+++ tecboard/src/components/FormularioDeEvento/index.jsx
@@ -3,6 +3,8 @@ import { TituloFormulario } from "../TituloFormulario";
 import { CampoDeFormulario } from "../CampoDeFormulario";
 import { Label } from "../Label";
 import { CampoDeEntrada } from "../CampoDeEntrada";
+import { Botao } from "../Botao";
+import { ListaSuspensa } from "../ListaSuspensa";
 
 export function FormularioDeEvento() {
   return (
@@ -19,11 +21,20 @@ export function FormularioDeEvento() {
             name="nomeEvento"
           />
         </CampoDeFormulario>
-        
+
         <CampoDeFormulario>
           <Label htmlFor="dataEvento">Data do Evento</Label>
           <CampoDeEntrada type="date" id="dataEvento" name="dataEvento" />
         </CampoDeFormulario>
+
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do Evento</Label>
+          <ListaSuspensa></ListaSuspensa>
+        </CampoDeFormulario>
+
+        <div className="acoes">
+          <Botao>Criar Evento</Botao>
+        </div>
       </div>
     </form>
   );
